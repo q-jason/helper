47f8e8177255cdf613fbafe36afe6486efea9ffe
q-jason <596908546@qq.com>
2019-12-26 13:22:03 +0800

    no message

---
+++ example/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>点击按钮，打开控制台看输出</h1>
+    <!-- log -->
+    <div class="group">
+      <h3 class="group-title">
+        漂亮的日志(console)
+      </h3>
+      <div class="group-body">
+        <button @click="log">
+          打印常规日志
+        </button>
+        <button @click="info">
+          打印信息日志
+        </button>
+        <button @click="warn">
+          打印警告日志
+        </button>
+        <button @click="err">
+          打印错误日志
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { log, info, warn, err } from '../src'
+
+  export default {
+    methods: {
+      log () {
+        log('用户改变了所在地区', [
+          '省份：山东省',
+          '城市：青岛市',
+          '地区：城阳区'
+        ])
+      },
+      info () {
+        info('接收到了后端给的数据，并格式化数据', [
+          { name: '姓名', age: 18, other: [ 1, 2, 3 ] },
+          { name: '班级 - 姓名', age: '18岁', other: [ 1, 2, 3 ] }
+        ])
+      },
+      warn () {
+        warn('转换后端数据时发现问题, 缺少字段', [
+          '缺少字段的数据',
+          { name: '姓名', age: 18, other: [ 1, 2, 3 ] }
+        ])
+      },
+      err () {
+        err('请求接口失败，打印出错误原因', [
+          new Error('失败了')
+        ])
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .group {
+    padding: 30px;
+  }
+  .group:not(:last-child) {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1)
+  }
+  button:not(:last-child) {
+    margin-right: 15px;
+  }
+</style>
---
+++ src/modules/beautiful-log.js
@@ -0,0 +1,86 @@
+/**
+ *  优化控制台输出日志样式
+ *  使其醒目易懂
+ *  注意：使用了下面方法后，游览器控制台将无法定位到代码位置
+ *
+ *  所有方法参数如下
+ *  @param { String } title - 日志标题，会显示在当前日志的头部，根据不同方法会有不同背景色，醒目显示
+ *  @param { String | Array } descArr - 详解介绍，可以为数组，会显示多行
+ **/
+
+/** 各方法主题色 **/
+const COLOR_LOG = '#4CAF50'
+const COLOR_INFO = '#2196F3'
+const COLOR_WARN = '#F57C00'
+const COLOR_ERR = '#FF5252'
+
+/** 内部实现方法 **/
+const innerHandler = function (title, descArr, color) {
+  let titleIsStr = typeof title === 'string'
+  let descArrIsArr = descArr instanceof Array
+  let descArrIsStr = typeof descArr === 'string'
+  
+  /** 参数检查 **/
+  if (titleIsStr === false) {
+    throw new TypeError('title 必须是 string 类型')
+  }
+  else if (descArrIsArr === false && descArrIsStr === false) {
+    throw new TypeError('descArr 必须是 String 或 Array 类型')
+  }
+  
+  /** 统一 descArr 为数组类型 **/
+  if (descArrIsStr) {
+    descArr = [ descArr ]
+  }
+  
+  /** 输出漂亮的日志 **/
+  console.log('=============================================================')
+  console.log(`%c${ title }`, `color: white; font-size: 12px; background-color: ${ color }; padding: 2px 5px; border-radius: 2px`)
+  console.log('----')
+  descArr.forEach(function (desc) {
+    console.log(desc)
+  })
+  console.log('----')
+  console.log('=============================================================')
+  console.log('')
+  console.log('')
+}
+
+/**
+ *  普通日志
+ *  场景：功能简短介绍
+ **/
+export const log = function (title, descArr) {
+  innerHandler(title, descArr, COLOR_LOG)
+}
+
+/**
+ *  信息日志
+ *  场景：用于打印数据
+ **/
+export const info = function (title, descArr) {
+  innerHandler(title, descArr, COLOR_INFO)
+}
+
+/**
+ *  警示日志
+ *  场景：用于非致命性错误，但是需要注意
+ ***/
+export const warn = function (title, descArr) {
+  innerHandler(title, descArr, COLOR_WARN)
+}
+
+/***
+ *  错误日志
+ *  场景：致命错误
+ ***/
+export const err = function (title, descArr) {
+  innerHandler(title, descArr, COLOR_ERR)
+}
+
+export default {
+  log,
+  info,
+  warn,
+  err
+}
